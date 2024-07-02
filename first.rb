59f0e8d37e5fab00c74bd38f1abe66c907c1ed6d
Oxana Michkasova <oxana.mitch@gmail.com>
2024-07-01 22:00:10 -0400

    Practice first program in ruby

---
+++ first.rb
@@ -0,0 +1,5 @@
+first_variable = 7
+
+puts "Enter your name." # Here we put a prompt to the console
+name = gets.chomp       # Here we collect the answer
+puts "Well hello, #{name}!"  # We respond with a greeting
